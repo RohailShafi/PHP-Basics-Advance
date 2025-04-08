95727df88adaa2a2657015e664196991115a5090
Rohail Shafi <rohailahmad2001@gmail.com>
2025-04-08 11:01:15 +0500

    echo , print print_r, variables, PHP in html

---
+++ echo.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "<b><i> Muhammad Shafi</b> </i>", "<b>Do you know me</b>";
+
+echo nl2br("\nRohail Ahmad \n") ;
+
+echo nl2br('Rohail here');
+echo nl2br("\n Rohail\n");
+
+echo "Rohail Shafi" , (" Bilal Shafi\n");
+
+echo nl2br("\nShoiab" . " Shafi\n");
+echo 567;
+echo nl2br("\n");
+echo nl2br(34566);
+
+
+print "<b><i> Muhammad Shafi</b></i> ";
+?>
---
+++ first.php
@@ -0,0 +1,12 @@
+<html lang="en">
+    <head>
+        <title>Rohail learning PHP Basics</title>
+    </head>
+    <body>
+    <?php
+
+    echo 'Hello Rohail Are you here??';
+
+    ?>
+    </body>
+</html>
